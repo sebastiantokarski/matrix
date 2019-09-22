1432eeb8e6353577aaf122323e45377ef621fbba
sebastiantokarski <sebastiantokarski01@gmail.com>
2019-09-22 10:03:19 +0200

    Initial app configuration

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const SOURCE_DIR = path.resolve(__dirname, 'src');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  mode: 'development',
+  entry: SOURCE_DIR,
+  output: {
+    path: DIST_DIR,
+    filename: 'app.js',
+  },
+  watch: true,
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
+      options: {
+        emitWarning: true,
+        configFile: './eslintrc.json',
+      }
+    }, {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+          ]
+        }
+      }
+    }]
+  }
+}
