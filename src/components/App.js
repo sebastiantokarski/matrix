aecc54c3782df33f1f4ea3606c0fa1dc8989f4a2
Sebastian Tokarski <stokarski@efigence.com>
2019-09-23 18:03:44 +0200

    Chart unit change functionality; format labels and chart units

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,22 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this.matrixData = matrix;
-    this.currMouseOverEl = null;
-
     this.state = {
       histogramChartHour: null,
+      averageChartUnitType: 'value',
     };
 
     this.handleMouseOver = this.handleMouseOver.bind(this);
     this.showHistogramWithDelay = this.showHistogramWithDelay.bind(this);
     this.getAverageForEveryHour = this.getAverageForEveryHour.bind(this);
     this.getRoundValuesForEveryHour = this.getRoundValuesForEveryHour.bind(this);
+    this.onUnitTypeChartChange = this.onUnitTypeChartChange.bind(this);
+
+    this.matrixData = matrix;
+    this.currMouseOverEl = null;
+
+    this.averageValueForEveryHour = this.getAverageForEveryHour();
+    this.roundValuesForEveryHour = this.getRoundValuesForEveryHour();
   }
 
   /**
@@ -56,6 +61,9 @@ export default class App extends Component {
     return hours;
   }
 
+  /**
+   * @param {Event} e
+   */
   handleMouseOver(e) {
     this.currMouseOverEl = e.target.x;
     this.showHistogramWithDelay(this.currMouseOverEl, 300);
@@ -76,20 +84,45 @@ export default class App extends Component {
     }, delay);
   }
 
-  render() {
-    const averageValueForEveryHour = this.getAverageForEveryHour();
-    const roundValuesForEveryHour = this.getRoundValuesForEveryHour();
+  onUnitTypeChartChange(e) {
+    this.setState({
+      averageChartUnitType: e.target.value,
+    });
+  }
 
+  render() {
     return (
       <div className="container">
+        <div className="chart-unit">
+          <fieldset className="type">
+            <input
+              type="radio"
+              name="type"
+              value="value"
+              id="chart-unit-value"
+              onChange={this.onUnitTypeChartChange}
+              defaultChecked />
+            <label
+              htmlFor="chart-unit-value">Show values</label>
+            <input
+              type="radio"
+              name="type"
+              value="percent"
+              id="chart-unit-percent"
+              onChange={this.onUnitTypeChartChange} />
+            <label
+              htmlFor="chart-unit-percent">Show percent</label>
+          </fieldset>
+        </div>
         <AverageLineChart
-          data={averageValueForEveryHour}
+          data={this.averageValueForEveryHour}
+          unitType={this.state.averageChartUnitType}
           onMouseOver={this.handleMouseOver}
         />
         {
           this.state.histogramChartHour !== null
           && <HistogramChart
-            data={roundValuesForEveryHour[this.state.histogramChartHour]}
+            data={this.matrixData[this.state.histogramChartHour]}
             hour={this.state.histogramChartHour} />
         }
       </div>
