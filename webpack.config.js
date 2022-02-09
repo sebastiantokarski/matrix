f49fbc974ebff17f9e88f12ed0536bd8701ea4dd
Sebastian Tokarski <sebastian.tokarski@idea.edu.pl>
2022-02-09 20:58:20 +0100

    fix production bundling

--- webpack.config.js
+++ webpack.config.js
@@ -3,52 +3,53 @@ const fs = require('fs')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const SOURCE_DIR = path.resolve(__dirname, 'src')
-const DIST_DIR = path.resolve(__dirname, 'dist')
-
 const appDirectory = fs.realpathSync(process.cwd())
 const resolveAppPath = (relativePath) =>
   path.resolve(appDirectory, relativePath)
 
-module.exports = {
-  mode: 'development',
-  entry: SOURCE_DIR,
-  output: {
-    filename: 'static/js/bundle.js',
-  },
-  devServer: {
-    static: {
-      directory: resolveAppPath('public'),
+module.exports = (env) => {
+  const mode = env.mode === 'production' ? 'production' : 'development'
+
+  return {
+    mode,
+    entry: resolveAppPath('src'),
+    output: {
+      filename: 'static/js/bundle.js',
     },
-    compress: true,
-    port: 3000,
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+    devServer: {
+      static: {
+        directory: resolveAppPath('public'),
+      },
+      compress: true,
+      port: 3000,
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new ESLintPlugin({
+        exclude: ['node_modules', 'src/data/*.js'],
+      }),
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: resolveAppPath('public/index.html'),
+      }),
     ],
-  },
-  plugins: [
-    new ESLintPlugin({
-      exclude: ['node_modules', 'src/data/*.js'],
-    }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: resolveAppPath('public/index.html'),
-    }),
-  ],
+  }
 }
